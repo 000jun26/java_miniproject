e624ee032e44729b4a951d6a18991abc777d103f
park <hsooo01@naver.com>
2023-12-10 19:20:18 +0900

    first

---
+++ src/BaseBallGame/Field.java
@@ -0,0 +1,295 @@
+package BaseBallGame;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Vector;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Field extends JFrame {
+// private Timer timer; // 시간 지연에 필요한 Timer 객체.
+private HitMap Hit_Frame; // Hitmap 프레임
+HitMap.ballThread ballthread; // HitMap 클래스의 내부 클래스인 ballThread클래스의 객체 ballthread 를 생성.
+private String team_name; // 팀 이름
+private Color[] baseColors = { Color.WHITE, Color.WHITE, Color.WHITE }; // 타격 결과에 따른 베이스 색 변화를 주기위한 베이스 색 배열.
+private Boolean[] cur_Base = { false, false, false }; // 현재 1-2-3 루 베이스에 진출한 주자들의 유-무를 판별해주는 배열.
+// 주자 진출 알고리즘에 필수적인 배열이다. true-false 로, 해당 베이스에 주자가 있는지. 아니면 비어있는 베이스인지 판별할 수
+// 있다.
+// 1-2-3 루 베이스로만 구성(3개의 요소). 홈 베이스는 구성x. 3루에서 일반 진출 혹은 2루에서 2루타로 진출시 그냥 득점으로
+// 처리하고
+// 기존 주자가 있던 해당 베이스는 false 로하여 비어있도록 표시한다. cur_Base 배열의 각 요소 값을 설정 후 각 베이스의 색깔을
+// baseColors 배열에 반영하고 repaint 하여 베이스 색을 최종 변경하여 베이스 상태를 field 프레임에서 눈으로 확인이
+// 가능해지는
+// 주자 진출 알고리즘을 고안해내었다.
+// 당연히 초기 cur_Base 요소들의 값은 false 로, 모든 베이스에는 주자가 없는 상태이다.
+private int score[][] = new int[2][9]; //점수를 나타내는 배열 score[0]은 플레이어팀 점수 score[1]은 상대팀 점수
+JLabel scoreLabel[][] = new JLabel[2][10];  //점수판에 점수를 나타내는 Label
+
+
+public void timeStop_changeFrame(int second) {
+	Timer timer = new Timer(second, new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			changeFrame();
+		}
+	});
+
+	timer.setRepeats(false);
+	timer.start();
+
+}
+
+public void changeFrame() { // hit 프레임으로 화면 전환 메서드. 1.2 초 뒤에 전환됨.(주루 상황 표시 후 다시 히트프레임으로 전환)
+
+
+	Hit_Frame.setVisible(true);
+	dispose();
+}
+
+public void initScore() {  //점수 초기화
+	for(int j=0;j<score.length;j++) {
+		for(int i=0;i<score[0].length;i++)
+			score[j][i]=0;
+	}
+}
+public void printScore(int inning) {  //점수판에 점수 출력
+	int sum=0,sum2=0;
+	scoreLabel[0][inning].setText(Integer.toString(score[0][inning]));
+	scoreLabel[1][inning].setText(Integer.toString(score[1][inning]));
+	for(int i=0;i<score[0].length;i++) {
+		sum += score[0][i];
+		sum2 += score[1][i];
+	}
+	scoreLabel[0][9].setText(Integer.toString(sum));
+	scoreLabel[1][9].setText(Integer.toString(sum2));
+}
+
+public void inningClose() { //이닝 종료
+	for (int i = 0; i < 3; i++)
+		cur_Base[i] = false;
+	score[1][Hit_Frame.inningCount-1]=(int)(Math.random()*(score[0][Hit_Frame.inningCount-1]+4));
+	//scoreLabel[1][Hit_Frame.inningCount-1].setText(Integer.toString(score[1][Hit_Frame.inningCount-1]));
+	printScore(Hit_Frame.inningCount-1);
+	set_CurBase();
+}
+// 이런식으로, 타격 결과에 따라서 현재 각 베이스의 상태를 바꿔준다.cur_Base 요소 값 변경.
+public void single_hit() {
+	if (cur_Base[2] == true) { // 3루에 주자가 있다면 홈으로 들어옴
+		cur_Base[2] = false;
+		// 점수 추가
+		score[0][Hit_Frame.inningCount]++;
+	}
+	if (cur_Base[1] == true) { // 2루에 주자가 있다면 3루로 진루
+		cur_Base[1] = false;
+		cur_Base[2] = true;
+	}
+	if (cur_Base[0] == true) { // 1루에 주자가 있다면 2루로 진루
+		cur_Base[0] = false;
+		cur_Base[1] = true;
+	}
+	cur_Base[0] = true; // 타자는 1루로 진루
+	//scoreLabel[0][Hit_Frame.inningCount].setText(Integer.toString(score[0][Hit_Frame.inningCount]));	
+	printScore(Hit_Frame.inningCount);
+	set_CurBase();
+}
+
+public void double_hit() {
+	if (cur_Base[1] == true) { // 3루 혹은 2루에 주자가 있다면 홈으로 들어옴
+		cur_Base[1] = false;
+		// 점수 추가
+		score[0][Hit_Frame.inningCount]++;
+	}
+	if(cur_Base[2]==true) {
+		cur_Base[2]=false;
+		score[0][Hit_Frame.inningCount]++;
+	}
+	if (cur_Base[0] == true) { // 1루에 주자가 있다면 3루로 진루
+		cur_Base[0] = false;
+		cur_Base[2] = true;
+	}
+	cur_Base[1] = true; // 타자는 2루로 진루
+	
+	//scoreLabel[0][Hit_Frame.inningCount].setText(Integer.toString(score[0][Hit_Frame.inningCount]));
+	printScore(Hit_Frame.inningCount);
+	set_CurBase();
+}
+
+public void homerun() {
+	for (int i = 0; i < 3; i++) {
+		if (cur_Base[i] == true) { // 모든 베이스에 주자가 있다면 홈으로 들어옴
+			cur_Base[i] = false;
+			// 점수 추가
+			score[0][Hit_Frame.inningCount]++;
+		}
+	}
+	// 타자도 홈으로 들어와 점수 추가
+	score[0][Hit_Frame.inningCount]++;
+	//scoreLabel[0][Hit_Frame.inningCount].setText(Integer.toString(score[0][Hit_Frame.inningCount]));
+	printScore(Hit_Frame.inningCount);
+
+	set_CurBase();
+}
+
+// 현재 베이스의 주자 진출 유-무에 따라서 베이스 색을 설정해주는 메서드. 즉, 현재 베이스에 주자 진출 유-무를 확인할 수 있도록해주는
+// 메서드.
+public void set_CurBase() {
+	for (int i = 0; i < cur_Base.length; i++) {
+		if (cur_Base[i] == false) // 이 베이스에 주자가 없으면 베이스를 흰색으로 설정.
+			baseColors[i] = Color.WHITE;
+		else // 이 베이스에 주자가 있으면 베이스를 파란색으로 설정.
+			baseColors[i] = Color.BLUE;
+	}
+
+	repaint(); // 베이스 색 설정 후, repaint 호출하여 색 변경을 적용시킨다.
+
+	timeStop_changeFrame(2000); // 3초간 시간 지연. 베이스 상태 변경 후 (repaint), hitMap 프레임으로 화면 전환.
+	// 모르겠다. hitmap 프레임의 timeStop_hit_changeFrame(2000); 시간 값과, 
+	//현재 field 프레임의 timeStop_changeFrame(4500); 의 시간값을 조율하니 잘 된다. 
+	//아마도 timer 객체간의 충돌? 이거나 별도의 타이머 스레드 개념이다 보니 병렬적으로 실행되기때문에  
+	//타이머가 즉 시간 지연이, sequential 하게 흘러가는것이 아닌것 같다.
+	
+	//즉, HitMap 프레임에서 timeStop_hit_changeFrame(2000); 이 실행되어 2초간 멈추고 현재 Field 프레임으로 화면 전환이 되는동안
+	//그 2초 기다리는 동안에 field_Frame.single_hit(); 이 실행되어 
+
+}
+// -------------경기장 패널------------------//
+
+class fieldPanel extends JPanel {
+
+	private Image basefield = new ImageIcon("images/base_field.jpg").getImage();
+	private Image scoreboard = new ImageIcon("images/scoreboard.png").getImage();
+	private Vector<int[]> baseX; // 123루+홈 베이스를 구성할때 필요한 배열을 요소로하는 벡터 baseX.
+	private Vector<int[]> baseY; // 123루+홈 베이스를 구성할때 필요한 배열을 요소로하는 벡터 baseY.
+
+	// 베이스 배열 구성 메서드
+	public void baseArrays() {
+		baseX = new Vector<int[]>();
+		baseY = new Vector<int[]>();
+		int[] x1 = { 660, 620, 660, 700 };
+		int[] y1 = { 410, 450, 490, 450 };
+		int[] x2 = { 490, 450, 490, 530 };
+		int[] y2 = { 270, 310, 350, 310 };
+		int[] x3 = { 320, 280, 320, 360 };
+		int[] y3 = { 410, 450, 490, 450 };
+		// 홈 베이스는 없어도될듯? 3루에서 진루해서 홈 밟는거는 3루 흰색으로 변경해서
+		// 홈으로 진루해서 득점한 것으로 표현해도될듯.
+		baseX.add(x1);
+		baseY.add(y1);
+		baseX.add(x2);
+		baseY.add(y2);
+		baseX.add(x3);
+		baseY.add(y3);
+
+	}
+
+	public fieldPanel() {
+		// TODO Auto-generated constructor stub
+
+		setLayout(null);
+		baseArrays(); // 베이스 배열 구성.
+		for(int i=0;i<scoreLabel[0].length;i++) {//점수판에 점수를 나타내는 기능
+			for(int j=0;j<scoreLabel.length;j++) {
+			scoreLabel[j][i] = new JLabel("");
+			scoreLabel[j][i].setSize(30,30);
+			if(i==scoreLabel[0].length-1)
+				scoreLabel[j][i].setLocation(240+37*i,50*(j+1));
+			else
+				scoreLabel[j][i].setLocation(224+37*i,50*(j+1));
+			scoreLabel[j][i].setFont(new Font("gothic",Font.BOLD , 20));
+			scoreLabel[j][i].setForeground(Color.WHITE);
+			add(scoreLabel[j][i]);
+			}
+		}
+		JButton btn = new JButton("Play Ball");
+		btn.setLocation(610, 0);
+		btn.setSize(100, 50);
+		this.add(btn);
+		btn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				// 고급 스윙기술 => JOptionPane.showInputDialog 사용하여 팀 이름 입력받음
+				 team_name = JOptionPane.showInputDialog("팀 이름을 선택해주세요=>");
+				 JLabel lb= new JLabel(team_name);
+				 lb.setAlignmentX(CENTER_ALIGNMENT);
+				 lb.setAlignmentY(CENTER_ALIGNMENT);
+				 lb.setSize(100, 30);
+				 lb.setLocation(50, 50);
+				 lb.setFont(new Font("gothic",Font.BOLD , 20));
+				 lb.setForeground(Color.WHITE);
+				 add(lb);
+				 JLabel lb2= new JLabel("적팀");
+				 lb2.setAlignmentX(CENTER_ALIGNMENT);
+				 lb2.setAlignmentY(CENTER_ALIGNMENT);
+				 lb2.setSize(100, 30);
+				 lb2.setLocation(60, 100);
+				 lb2.setFont(new Font("gothic",Font.BOLD , 20));
+				 lb2.setForeground(Color.WHITE);
+				 add(lb2);
+				ballthread.start();
+				Hit_Frame.setVisible(true);
+				dispose(); // dispose메서드로 현재 field 프레임은 해제하고 HitMap 프레임을 visible하도록하여 화면 전환 효과.
+				remove(btn); // btn.setvisible(false) 도 가능.
+				// revalidate(); 혹시 제거후 반영 안될 시 프레임 재배열 + 다시 그리기
+				// repaint();
+
+			}
+		});
+
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paintComponent(g);
+		g.drawImage(basefield, 0, 0, this.getWidth(), this.getHeight(), this);
+		g.drawImage(scoreboard, 0, 0, 600, 150, this);
+
+		g.setColor(Color.WHITE);
+
+		for (int i = 0; i < baseX.size(); i++) {
+			g.setColor(baseColors[i]);
+			int[] x = baseX.get(i);
+			int[] y = baseY.get(i);
+			g.fillPolygon(x, y, 4); // 베이스 그리기.
+		}
+	}
+
+}
+
+public Field() {
+	// TODO Auto-generated constructor stub
+	initScore();
+	this.setTitle("한성 스타디움");
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setSize(1000, 700);
+	// setLocation(500, 200);
+	setLocation(500, 200);
+	Hit_Frame = new HitMap(this); // newFrame 에게 지금이 startFrame 에 대한 정보를 전달.
+
+	ballthread = Hit_Frame.new ballThread();
+	Hit_Frame.ballthread = ballthread; // Hit_Frame 객체의 ballthread스레드를 현재 Field 객체의 ballthread 로 설정.
+										// 즉, HitMap 과 Field 가 동일한 ballthread스레드를 공유하게 됐다!!
+
+	fieldPanel fPanel = new fieldPanel();
+	setContentPane(fPanel);
+	setVisible(true);
+
+}
+
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	Field field_Frame = new Field();
+}
+}
---
+++ src/BaseBallGame/HitMap.java
@@ -0,0 +1,372 @@
+package BaseBallGame;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class HitMap extends JFrame {
+
+	boolean swing = false; //베트를 휘둘렀는지 논리변수.
+	int inningCount = 0; //이닝 카운트 
+	int strikeCount = 0; //스트라이크 카운트
+	int outCount = 0; //아웃 카운트
+	ImageIcon normalBatIcon = new ImageIcon("images/bat.png"); // 기본 베트 이미지
+	ImageIcon swingBatIcon = new ImageIcon("images/swingBat.gif");
+	Field field_Frame;
+
+	JLabel b_ball = new JLabel(new ImageIcon("images/b_ball.png"));
+	JLabel[] countLight = new JLabel[4]; // 볼 스트라이크 아웃 카운트 레이블 배열
+	JLabel hitZone = new JLabel(normalBatIcon);
+	ballThread ballthread; // 공 날라오는 스레드
+	JLabel lbl; // 경기 시작, 투수가 공 던지기까지 대기하는 와인드업 타임, 볼 -스트- 아웃 알려주는 레이블
+	String[] hitTypeArray = { "single", "double", "homerun" }; // 타격 성공시, 타격의 종류를 나타내는 문자열 배열.
+	// 더블 플레이는 구현이 어려워 당장은 배제. 추후 추가 가능.
+	
+	
+	//이닝 처리 알고리즘
+	public void inningAlgorithm() {
+		inningCount++;
+		
+		//상대 팀 공격 자동화. showmessageDialog() 로 상대팀 득점 결과 나오도록
+		
+		if(inningCount==9) {  //0부터 시작하여 8까지 배열때문에 
+			System.exit(0);
+		}
+	}
+	
+	//스트라이크 처리 알고리즘
+	public void strikeAlgorithm() {
+	    strikeCount++; // 스트라이크 카운트 추가
+	    ballthread.pauseThread();
+	    b_ball.setLocation(500, 0);
+	    
+	    if (strikeCount <= 2) {
+	        countLight[strikeCount - 1].setVisible(true); // 스트라이크 카운트 라이트 켜기
+	        lbl.setText("STRIKE "+strikeCount);
+	    } 
+	    
+	    else if (strikeCount == 3) { // 3 스트라이크 => 아웃 카운트 ++
+	        strikeCount = 0; // 스트라이크 카운트 초기화
+	        countLight[0].setVisible(false);
+	        countLight[1].setVisible(false);
+	        outCount++;
+	        
+	        if (outCount <= 2) {
+	            countLight[1 + outCount].setVisible(true); // 아웃 카운트 라이트 켜기
+	            lbl.setText("OUT "+outCount);
+	        } 
+	        else if (outCount == 3) {
+	            lbl.setText("inning end!!!");
+	            outCount = 0; // 아웃 카운트 초기화
+	            countLight[2].setVisible(false);
+	            countLight[3].setVisible(false);
+	            inningAlgorithm();  //이닝++
+	            timeStop_hit_changeFrame(2000,"");  //이 함수에서 이닝교체 구현해놓음 else부분에
+	        }
+
+	    }
+	    hitZone.setIcon(normalBatIcon);
+	}
+	//베트를 휘두르지 않았을 경우, 즉 루킹 스트라이크 당했을경우 스트라이크 처리를 위한 메서드
+	public void strikeTimestop_swingN(int second) {
+		Timer timer = new Timer(second, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				strikeAlgorithm();
+			}
+		});
+
+		timer.setRepeats(false);
+		timer.start();
+
+	}
+	//베트를 휘둘렀을 경우, 즉 헛스윙한 경우 스트라이크 처리를 위한 메서드.
+	//h 키를 눌러서 swing 값이 true 가 된다음, 다시 false 로 값을 바꿔줘야하기에.
+	//h 키를 눌러 swing 값이 true가 된 후에 false 로 다시 안바꿔주면 swing 변수는 계속 true 값이되어
+	//헛스윙 + 루킹 스트라이크 중복되어 strikeAlgorithm()가 동시에 두번 호출되기에 오류가 난다. 
+	//so, 별도의 strikeTimestop_sw() 라는 헛스윙시의 별도의 메서드를 만들어준 것이다.
+	public void strikeTimestop_swingY(int second) {
+		Timer timer = new Timer(second, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				strikeAlgorithm();
+				swing=false;
+			}
+		});
+		
+		timer.setRepeats(false);
+		timer.start();
+		
+	}
+	
+	public String random_HitType() { // 랜덤한 타격 종류 선택하여 타격 종류의 문자열을 리턴해주는 메서드
+		int randomNum = (int) (Math.random() * hitTypeArray.length);
+		return hitTypeArray[randomNum];
+	}
+	
+
+	public void timeStop_hit_changeFrame(int second, String s) {
+		Timer timer = new Timer(second, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String HitType = s;
+				swing=false;
+				hit_changeFrame();
+
+				if (HitType.equals("single"))
+					field_Frame.single_hit();
+				else if (HitType.equals("double"))
+					field_Frame.double_hit();
+				else if (HitType.equals("homerun"))
+					field_Frame.homerun();
+				else
+					field_Frame.inningClose();
+				/*
+				 * else if(random_HitType().equals("fly_out")) field_Frame.fly_out(); else
+				 * field_Frame.ground_out();
+				 */
+
+				// 이러면 2초 대기후 화면 전환 후 => field 에서 타격으로인한 베이스 진루 적용 후 4.5 대기 후 다시 이곳으로 전환됨.
+				// 추후, random 으로 sigle or double or 홈런 or 땅볼아웃 플라이아웃으로.
+				lbl.setText("ready for the next ball..");
+				hitZone.setBackground(new Color(150, 75, 0));
+				b_ball.setLocation(500, 0);
+				hitZone.setIcon(normalBatIcon);
+			}
+		});
+
+		timer.setRepeats(false);
+		timer.start();
+
+	}
+
+	public void hit_changeFrame() { // 탸격 성공시의 야구장 필드 프레임으로 화면 전환 메서드. 1.2 초 뒤에 전환됨.( 타격 결과 확인을 위한 텀)
+		
+		field_Frame.setVisible(true);
+		dispose();
+        countLight[0].setVisible(false);
+        countLight[1].setVisible(false);
+	}
+
+	public void normal_changeFrame() { // 야구장 필드 프레임으로 화면 전환 메서드.
+
+		field_Frame.setVisible(true);
+		dispose();
+	}
+
+	class HitPanel extends JPanel {
+		
+		
+		Image countboard = new ImageIcon("images/countboard.png").getImage();
+		Image hitter = new ImageIcon("images/hitter.png").getImage();
+		
+		public HitPanel() {
+			// TODO Auto-generated constructor stub
+
+			setLayout(null);
+
+			lbl = new JLabel("Play Ball.. get ready");
+			lbl.setLocation(600, 150);
+			lbl.setSize(400, 200);
+			lbl.setForeground(Color.RED);
+			lbl.setFont(new Font("jokerman", Font.BOLD, 30));
+			this.add(lbl);
+
+			JButton btn = new JButton("돌아가기");
+			btn.setLocation(850, 0);
+			btn.setSize(100, 50);
+			this.add(btn);
+			btn.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					normal_changeFrame();
+					// setVisible(false);
+				}
+			});
+
+			// 야구공 레이블
+			b_ball.setSize(30, 30);
+			b_ball.setLocation(500, 0);
+			add(b_ball);
+			// 히트존 레이블
+			hitZone.setSize(300, 300);
+			hitZone.setLocation(400, 400);
+			// hitZone.setBackground(new Color(150, 75, 0));
+			// hitZone.setOpaque(true);
+			add(hitZone);
+
+			/// 아웃 스트라이크 볼 카운트 레이블들 구성 및 패널에 부착
+			for (int i = 0; i < 4; i++) {
+				if (i <= 1) {
+					countLight[i] = new JLabel(new ImageIcon("images/STRIKE.png")); // countLight 0,1,2 은 볼 카운트 라이트
+					countLight[i].setLocation(49 + i * 47, 68);
+
+				} else {
+					countLight[i] = new JLabel(new ImageIcon("images/OUT.png"));// countLight 5,6 은 볼 카운트 라이트
+					countLight[i].setLocation(185 + i * 44, 68);
+				}
+				countLight[i].setSize(30, 30);
+				add(countLight[i]);
+				countLight[i].setVisible(false); // 초기 카운트보드 라이트의 상태는 비어있음.
+			}
+
+			setFocusable(true); // HitPanel 에게 키포커스 주기.
+			addKeyListener(new KeyAdapter() {
+
+				@Override
+				public void keyPressed(KeyEvent e) {
+					if (e.getKeyChar() == 's') {
+						// ballthread.interrupt();
+						// ballthread.start(); // 테스트용으로 s 누르면 공 스레드 시작
+
+					}
+					if (e.getKeyChar() == 'd') {
+						ballthread.pauseThread(); // 테스트용으로 s 누르면 공 스레드 일시중지
+					}
+
+					if (e.getKeyChar() == 'r') {
+						ballthread.resumeThread(); // 테스트용으로 s 누르면 공 스레드 재개
+						// batThread.start();
+					}
+
+					if (e.getKeyChar() == 'b') {
+						// ballthread.interrupt();
+						b_ball.setLocation(500, 0);
+					}
+					if (!swing) { //h 키 연타를 방지하기위해 swing 변수 이용.
+								  // 야구에서도 베트는 한번만 스윙가능하니깐.
+								  //즉, 논리변수를 통해 베트는 한번만 스윙할 수 있도록 구현하였다.
+					if (e.getKeyChar() == 'h') {
+						swing=true; // h 키를 눌러서 베트를 스윙함.
+						hitZone.setIcon(swingBatIcon);
+						
+						/// **************** 이 아래 if 문이 바로 타격 성공시 !!! *****************//
+						if (b_ball.getY() >= 550 - 30 && b_ball.getY() <= 550 + 5) { // 히트존
+																						// 설정.어느정도 Y
+																						// 축까지를
+																						// 히트존으로
+							
+							// 즉, 타격 성공 구역으로 설정할지는 -50 +10 수정하여 조절가능.
+							ballthread.pauseThread();// 타격 성공시 공이 멈춤.
+							// 위에걸 지우고 공이 멈추는게 아니라 날아가도록 구현해보자
+							
+							
+							
+							String hitType = random_HitType();
+							lbl.setText(hitType);
+							strikeCount = 0; // 안타 시 스트라이크 카운트 초기화
+					        countLight[0].setVisible(false);
+					        countLight[1].setVisible(false);
+
+							timeStop_hit_changeFrame(2000, hitType); // 그리고 필드 화면으로 전환되어 필드 상태 확인.
+							// 이제 추가로, 타격 결과를 설정하고, 그에 따라 카운트 보드 및 베이스 진루 설정해야지
+							
+						} else// 헛스윙. h 키를 눌러서 스윙했지만 타격은 실패한 경우.
+						{
+							strikeTimestop_swingY(2000);
+						}
+					}
+
+				}
+				}
+			});
+		}
+
+		@Override
+		protected void paintComponent(Graphics g) {
+			// TODO Auto-generated method stub
+			super.paintComponent(g);
+			g.drawImage(countboard, 0, 0, 400, 100, this);
+			g.drawImage(hitter, 650, 450, 100, 200, this);
+
+		}
+	}
+
+	public HitMap(Field field_Frame) {
+		// TODO Auto-generated constructor stub
+		this.field_Frame = field_Frame;
+
+		setContentPane(new HitPanel());
+
+		this.setSize(1000, 700);
+		this.setTitle("타격시작");
+		this.setLocation(500, 200);
+	}
+
+	/// 공 스레드
+	class ballThread extends Thread {
+		Random rand = new Random(); // 랜덤 객체 생성
+		int hitZoneCenterX = hitZone.getX() + hitZone.getWidth() / 3; // hitZone의 중점 x 좌표
+		// 진폭과 주파수를 랜덤하게 설정
+
+		private boolean paused = true; // 스레드 일시 중지 여부를 체크하는 변수
+
+		public void pauseThread() { // 공 멈추기 메서드
+			paused = true; // 외부에서 호출하여 스레드를 일시 중지
+		}
+
+		public void resumeThread() { // 공 던지기 메서드
+			synchronized (this) {
+				paused = false;
+				notify(); // 스레드 재개를 위해 일시 중지 상태 해제
+			}
+		}
+
+		@Override
+		public void run() {
+			while (true) {
+				synchronized (this) {
+					if (paused) { // 일시 중지 플래그가 true이면 실행을 대기
+						try {
+							wait();
+						} catch (InterruptedException e) {
+							return;
+						}
+					}
+				}
+				
+				// 공의 y 좌표를 먼저 증가
+				int ballY = b_ball.getY() + 10;
+				b_ball.setLocation(b_ball.getX(), ballY);
+				 
+				if(ballY == 720 && swing==false) {
+					strikeTimestop_swingN(2000);
+				}
+				
+				double amplitude = hitZoneCenterX / 10.0; // 고정된 진폭 값으로 설정
+				double frequency = 2 * Math.PI / getHeight() * 0.5; // 고정된 주파수 값으로 설정
+
+				// 공의 x 좌표를 y 좌표의 사인 함수로 설정. hitZone의 중점을 중심으로 휘어져서 이동
+				int ballX = hitZoneCenterX - (int) (amplitude * Math.sin(frequency * ballY)); // 사인 함수에 "-1"을 곱해 반전
+				b_ball.setLocation(ballX, b_ball.getY());
+
+				try {
+					sleep(8); // 한 10~ 1 정도?
+				} catch (InterruptedException e) {
+					return;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
